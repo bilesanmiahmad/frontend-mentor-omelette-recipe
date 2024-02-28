d968ab380dcd4663c3db5ef6c67c0b27652c3e29
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2024-02-28 14:04:36 +0000

    recipe page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        youngserif: ["Young Serif", "serif"],
+        outfit: ["Outfit", "san-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
